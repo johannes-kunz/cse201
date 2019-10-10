eef9b379594260eb600fa75898c4df8fac37998e
johannes-kunz <johannes.kunz@polytechnique.edu>
2019-10-10 10:38:26 +0200

    finished exercises 1 and 2

---
+++ T4/ex1.cpp
@@ -0,0 +1,18 @@
+
+#include "Complex_Ex1.cpp"
+int main() {
+  Complex c1;
+  Complex c2 = Complex(3,4);
+
+  c1.normalize();
+
+  c1.setReal(c2.getReal());
+  c1.setImaginary(c2.getImaginary());
+
+  std::cout << "c1 magnitude: " << c1.magnitude() << std::endl;
+  c1.normalize();
+
+  std::cout << "c1 is: " << c1.getReal() <<
+    " + " << c1.getImaginary() << " * i" << std::endl;
+  std::cout << "c1 magnitude:" << c1.magnitude() << std::endl;
+}
